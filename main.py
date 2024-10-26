583f0d9068da581248aa60589e963052f640e9a8
Vignie Babul <vigniebabul.dev@gmail.com>
2024-10-26 13:53:48 +0200

    Properly named class

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import cli_menu
 
 if __name__ == '__main__':
 	options = [f'Option {i}' for i in range(1, 10)] + ['Exit']
-	menu = cli_menu._CLIMenu(options=options)
+	menu = cli_menu.CLIMenu(options=options)
 	print(menu.wait_response())
